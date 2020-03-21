0173d697ae5b747b164f69d05d589f872911d7a2
Gavin <gavin.cui@mail.utoronto.ca>
2020-03-21 15:01:15 -0400

    add missing ;

--- unwind.c
+++ unwind.c
@@ -325,7 +325,7 @@ queue_output(struct unwind_queue_t *queue, bool print)
 		free(tmp);
 	}
 	if (!print) {
-        uint32_t hash = murmur3_32((const uint8_t*)stack_buf, strlen(stack_buf), 2333)
+        uint32_t hash = murmur3_32((const uint8_t*)stack_buf, strlen(stack_buf), 2333);
         tprints(stack_buf);
         tprints("%lu\n", hash);
         line_ended();
