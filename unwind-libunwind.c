3a135da0183a8bee943bd2dd37a77413a8d8f849
Gavin <gavin.cui@mail.utoronto.ca>
2020-05-17 13:49:16 -0400

    test

--- unwind-libunwind.c
+++ unwind-libunwind.c
@@ -142,33 +142,6 @@ free_writable_region(struct proc_info* info)
 }
 
 void
-get_mem_region_addr_cache(struct tcb *tcp, struct proc_info* info)
-{
-    if (!tcp->mmap_cache){
-        // load mem_region_addr self
-        perror_msg("mmap_cache is NULL, try read maps file again!");
-        get_mem_region_addr(struct proc_info* info);
-        return;
-    }
-
-    for (int i = 0; i < tcp->mmap_cache->size; i++){
-        struct mmap_cache_entry_t *entry = &tcp->mmap_cache->entry[i];
-        // update proc_info, if address not match or is writable
-        if (info->regions[i].start_addr != entry->start_addr
-            || info->regions[i].end_addr != entry->end_addr
-            || entry->protections & MMAP_CACHE_PROT_WRITABLE) {
-            // record the addr range
-            free(info->regions[i].data);
-            info->regions[i].data = NULL;
-            info->regions[i].start_addr = start_addr;
-            info->regions[i].end_addr = end_addr;
-        }
-    }
-
-    info->num_regions = tcp->mmap_cache->size;
-}
-
-void
 get_mem_region_addr(struct proc_info* info)
 {
     rewind(info->map_fp);
@@ -231,6 +204,34 @@ get_mem_region_addr(struct proc_info* info)
     free(line_buf);
     line_buf = NULL;
 }
+
+void
+get_mem_region_addr_cache(struct tcb *tcp, struct proc_info* info)
+{
+    if (!tcp->mmap_cache){
+        // load mem_region_addr self
+        perror_msg("mmap_cache is NULL, try read maps file again!");
+        get_mem_region_addr(struct proc_info* info);
+        return;
+    }
+
+    for (unsigned int i = 0; i < tcp->mmap_cache->size; i++){
+        struct mmap_cache_entry_t *entry = &tcp->mmap_cache->entry[i];
+        // update proc_info, if address not match or is writable
+        if (info->regions[i].start_addr != entry->start_addr
+            || info->regions[i].end_addr != entry->end_addr
+            || entry->protections & MMAP_CACHE_PROT_WRITABLE) {
+            // record the addr range
+            free(info->regions[i].data);
+            info->regions[i].data = NULL;
+            info->regions[i].start_addr = entry->start_addr;
+            info->regions[i].end_addr = entry->end_addr;
+        }
+    }
+
+    info->num_regions = tcp->mmap_cache->size;
+}
+
 
 int
 _proc_access_mem (unw_addr_space_t as, unw_word_t addr, unw_word_t *val,
