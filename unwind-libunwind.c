14d243a1b254454d3f5198b12476b38813fd2d63
Gavin <gavin.cui@mail.utoronto.ca>
2020-06-08 01:41:37 -0400

    test

--- unwind-libunwind.c
+++ unwind-libunwind.c
@@ -278,9 +278,9 @@ _proc_access_mem (unw_addr_space_t as, unw_word_t addr, unw_word_t *val,
     // lazy load mem regions
     int index = find_mem_region(info, addr);
     if (index < 0) {
-//        int ret = _UPT_accessors.access_mem(as, addr, val, write, arg);
-//        perror_msg("can not find addr 0x%lx, use default ptrace, ptrace return = %d", addr, ret);
-        return -UNW_EINVAL;
+        int ret = _UPT_accessors.access_mem(as, addr, val, write, arg);
+        perror_msg("can not find addr 0x%lx, use default ptrace, ptrace return = %d", addr, ret);
+        return ret;
     }
     struct mem_region* region = &(info->regions[index]);
     // load the mem region if needed
@@ -341,7 +341,7 @@ init(void)
 	mmap_cache_enable();
 
 	unw_accessors_t proc_accessors = _UPT_accessors;
-//	proc_accessors.access_mem = _proc_access_mem;
+	proc_accessors.access_mem = _proc_access_mem;
 	libunwind_as = unw_create_addr_space(&proc_accessors, 0);
 
 	if (!libunwind_as)
